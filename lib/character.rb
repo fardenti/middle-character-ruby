30ae196522e6f939db96134265449eccea86a628
Student <student@Makerss-MacBook-Pro-17.local>
2019-09-09 17:50:11 +0100

    implemented odd character method

--- lib/character.rb
+++ lib/character.rb
@@ -1,6 +1,11 @@
 class Character
 
   def get_middle(word)
-    word[word.length/2-1] + word[word.length/2]
+    if (word.length.even? === true)
+      return word[word.length/2-1] + word[word.length/2]
+    else
+      return word[word.length/2]
+    end
   end
+
 end
--- spec/mc_spec.rb
+++ spec/mc_spec.rb
@@ -1,7 +1,13 @@
 require 'character'
+
 describe Character do
-  it 'brings the middles 2 characters of a word with even characters' do
+  it 'brings the middles 2 characters of a word with even character length' do
     character = Character.new
-    expect(character.get_middle("test")).to eq("es")
+    expect(character.get_middle("tester")).to eq("st")
+  end
+
+  it 'brings the middle 1 character of a word with odd character length' do
+    character = Character.new
+    expect(character.get_middle("testing")).to eq("t")
   end
 end
